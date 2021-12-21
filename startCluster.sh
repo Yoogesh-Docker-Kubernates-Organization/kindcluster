19d0308862b9dd5a6346736aa45e7085796eb8bc
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2021-12-21 10:09:58 -0500

    initial commit
    initial commit

---
+++ startCluster.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+################################# SETTING REPOSITORY PATH #########################
+REPOSITORY="/Users/ysharma/Documents/Git/"
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Default REPOSITORY is set to ${REPOSITORY}"
+  else
+    REPOSITORY=$1
+    echo "REPOSITORY is set to ${REPOSITORY}"
+fi
+echo "Also DON'T forget to provide the extraMounts hostpath for Jenkins and mysql at config.yaml file to define storage path on your local....👀 👀 👀 👀"
+#####################################################################################
+
+# cluster variable (Always true)
+start_cluster="true"
+
+#image creation variables
+create_jenkins_image="false"
+create_webapp_image="false"
+create_api_gateway_image="false"
+create_mfe_image="false"
+
+# deployment variables
+deploy_jenkins_image="true"
+deploy_webapp_image="true"
+deploy_api_gateway_image="true"
+deploy_mfe_image="true"
+
+# Constants
+CLUSTER=${REPOSITORY}kindcluster
+SPRING_BOOT_SECURITY=${REPOSITORY}springbootsecurity
+MY_ACCOUNT=${REPOSITORY}my-account
+REACT_MFE=${REPOSITORY}reactmfe
+
+
+############################ Start #######################################################
+if ${start_cluster} ne ''
+then
+   kind create cluster --name twm-digital --config ${CLUSTER}/config.yaml
+   echo "Sleeping for 5 sec 🌲 🇮🇳 🇳🇵 🈯️ 🏕"
+   sleep 5
+   
+   # Deploy the Kubernetes supported ingress NGINX controller to work as a reverse proxy and load balancer
+   # Additionally, we can also use AWS and GCE load balancer controllers
+   kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+fi
+
+########################################################
+# Creating all required images into a cluster
+########################################################
+
+# Jenkins image
+if ${create_jenkins_image} eq true
+then
+   docker image build -t myjenkins ${CLUSTER}/.
+fi
+
+# SpringbootSecurity image creation
+if ${create_webapp_image} eq true
+then
+   mvn -f ${SPRING_BOOT_SECURITY}/pom.xml clean install
+   docker image build -t yoogesh1983/springbootsecurity:latest ${SPRING_BOOT_SECURITY}/.
+fi
+
+# My-account image creation
+if ${create_api_gateway_image} eq true
+then
+   docker image build -t yoogesh1983/my-account:latest ${MY_ACCOUNT}/.
+fi
+
+# reactmfe image creation
+if ${create_mfe_image} eq true
+then
+   docker image build -t yoogesh1983/reactmfe:latest ${REACT_MFE}/.
+fi
+
+
+########################################################
+# loading And deploying all required images into a cluster
+########################################################
+
+# Jenkins deployment
+if ${deploy_jenkins_image} eq true
+then
+  kind load docker-image myjenkins --name twm-digital
+  kubectl apply -f ${CLUSTER}/jenkins.yaml
+fi
+
+# SpringBootSecurity deployment
+if ${deploy_webapp_image} eq true
+then
+  kind load docker-image yoogesh1983/springbootsecurity --name twm-digital
+  kubectl apply -f ${SPRING_BOOT_SECURITY}/src/main/resources/devops/k8s_aws/webapp/webApp.yaml
+fi
+
+# my-account deployment
+if ${deploy_api_gateway_image} eq true
+then
+  kind load docker-image yoogesh1983/my-account --name twm-digital
+  kubectl apply -f ${MY_ACCOUNT}/devops/webApp.yaml
+fi
+
+# reactmfe deployment
+if ${deploy_mfe_image} eq true
+then
+  kind load docker-image yoogesh1983/reactmfe --name twm-digital
+  kubectl apply -f ${REACT_MFE}/MFE/resources/devops/k8s_aws/webApp.yaml
+fi
+
+
+if ${start_cluster} eq true
+then
+   echo "Sleeping for 1 min 🌲 😴 🌲 🈯️ ✅ 💪🏽 👩🏻‍🦱 🧑🏾‍🦰"
+   sleep 60
+   
+   #Nginex controller
+   kubectl apply -f ${CLUSTER}/ingress.yaml
+   
+   echo "Cluster and Ingress successfully started"
+else
+  echo "Re-deployment of image is done successfully ..........👍 👍 👍"
+fi
+
+
